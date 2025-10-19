bd65b1f1523aae86da73db77e4e53a4d99c288be
Pranav <peddintipranav05@gmail.com>
2025-10-19 19:25:47 +0530

    signup section code rrefactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,8 +31,8 @@ export const signup = async (req,res) => {
          })
 
          if (newUser){
-            generateToken(newUser,res);
-            await newUser.save();
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id,res)
 
             res.status(201).json({
                 _id:newUser._id,
