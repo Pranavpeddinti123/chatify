194f240494e05ad20a0232e47739aa8d3250f3ff
Pranav <peddintipranav05@gmail.com>
2025-10-20 07:59:02 +0530

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,6 +67,10 @@ await sendWelcomeEmail(savedUser.email,savedUser.fullName,ENV.CLIENT_URL)
 
 export const login = async(req,res) => {
 
+    if(!email || !password){
+        return res.status(400).json({message:"Email and password are required"})
+    }
+
     const {email,password} = req.body;
 
     try{
