7fecc1061093d755bd87515adccda84dc090b1c3
Pranav <peddintipranav05@gmail.com>
2025-10-21 11:09:58 +0530

    new login and logout

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -17,7 +17,7 @@ export const useAuthStore = create((set, get) => ({
     try {
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
-      get().connectSocket();
+   
     } catch (error) {
       console.log("Error in authCheck:", error);
       set({ authUser: null });
@@ -33,7 +33,7 @@ export const useAuthStore = create((set, get) => ({
       set({ authUser: res.data });
 
       toast.success("Account created successfully!");
-      get().connectSocket();
+      
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -48,8 +48,6 @@ export const useAuthStore = create((set, get) => ({
       set({ authUser: res.data });
 
       toast.success("Logged in successfully");
-
-      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -62,7 +60,6 @@ export const useAuthStore = create((set, get) => ({
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
-      get().disconnectSocket();
     } catch (error) {
       toast.error("Error logging out");
       console.log("Logout error:", error);
